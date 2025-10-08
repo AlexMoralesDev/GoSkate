90484946ffaef28991603c2c0109447bc5155ee6
AlexMoralesDev <alex.morales8@upr.edu>
2025-10-08 08:52:33 -0400

    Switched to Flutter development

---
+++ GoSkate/GoSkate/GoSkateApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoSkateApp.swift
+//  GoSkate
+//
+//  Created by Alex Morales on 10/8/25.
+//
+
+import SwiftUI
+
+@main
+struct GoSkateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GoSkate/GoSkateTests/GoSkateTests.swift
@@ -0,0 +1,17 @@
+//
+//  GoSkateTests.swift
+//  GoSkateTests
+//
+//  Created by Alex Morales on 10/8/25.
+//
+
+import Testing
+@testable import GoSkate
+
+struct GoSkateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
