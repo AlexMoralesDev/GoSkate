fb00f71520a418cc372ccdc55f4bf70b6508f297
AlexMoralesDev <alex.morales8@upr.edu>
2025-10-08 08:57:41 -0400

    Simple bottom tab nav bar

--- GoSkate/GoSkate/ContentView.swift
+++ GoSkate/GoSkate/ContentView.swift
@@ -1,21 +1,50 @@
-//
-//  ContentView.swift
-//  GoSkate
-//
-//  Created by Alex Morales on 10/8/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            Page1()
+                .tabItem {
+                    Label("Home", systemImage: "house.fill")
+                }
+            
+            Page2()
+                .tabItem {
+                    Label("Search", systemImage: "magnifyingglass")
+                }
+            
+            Page3()
+                .tabItem {
+                    Label("Settings", systemImage: "gear")
+                }
         }
-        .padding()
+    }
+}
+
+struct Page1: View {
+    var body: some View {
+        VStack {
+            Text("Home Page")
+                .font(.largeTitle)
+        }
+    }
+}
+
+struct Page2: View {
+    var body: some View {
+        VStack {
+            Text("Search Page")
+                .font(.largeTitle)
+        }
+    }
+}
+
+struct Page3: View {
+    var body: some View {
+        VStack {
+            Text("Settings Page")
+                .font(.largeTitle)
+        }
     }
 }
 
